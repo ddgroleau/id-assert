f59b08b4f4725e2d05e9259756615cecdc9348dc
Dan Groleau <ddgroleau@gmail.com>
2022-07-05 11:27:56 -0600

    Initial commit.

---
+++ src/main/java/com/identity/api/IdAssertApplication.java
@@ -0,0 +1,13 @@
+package com.identity.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdAssertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdAssertApplication.class, args);
+	}
+
+}
